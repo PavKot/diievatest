ac8999ededf6370ce20754796a9d6989985ee308
PavKot <pavlo.melnik.20043@gmail.com>
2023-10-04 22:41:43 +0300

    updated banner

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -6,8 +6,10 @@ import banner from "../Assets/banner.png";
 const HeroSection = () => {
   return (
     <>
-      <div className="banner absolute left-0 right-0 mx-auto top-[25vh]">
-        <img src={banner} alt="" className="mx-auto" />
+      <div className="banner fixed left-0 right-0 mx-auto top-[25vh] z-[999]">
+        <a href="https://instagram.com/diieva_ua">
+          <img src={banner} alt="" className="mx-auto" />
+        </a>
       </div>
       <section className="w-[100%]">
         <img src={heroImgNew} alt="Hero Image" className="w-[100%]" />
