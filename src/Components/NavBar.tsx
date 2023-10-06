78510d708468f00e4661f2fa22d040391ac1628a
PavKot <pavlo.melnik.20043@gmail.com>
2023-10-06 15:54:00 +0300

    Update NavBar.tsx

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -50,7 +50,7 @@ const NavBarTest = () => {
       <Cart toggleCart={toggleCart} isOpen={isOpen} />
 
       <div className="backdrop-blur-5.6 w-full top-0 left-0 z-[999] mx-auto pt-[20px] sticky bg-blur md:p-0 p-[18px] md:py-2">
-        <div className="container mx-auto">
+        <div className="lg:px-10 2xl:px-0 container mx-auto">
           <div className="flex items-center justify-between">
             <ul
               className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[999] left-0 w-full md:w-auto md:pl-0 pl-0 w-[400px] ${
