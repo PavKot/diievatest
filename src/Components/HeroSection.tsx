4c260d84607f721f47ad21ea6965cc50ed81ddfa
PavKot <pavlo.melnik.20043@gmail.com>
2023-10-30 22:43:30 +0200

    no anims

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -12,88 +12,27 @@ const HeroSection = () => {
     { big: heroBig2, small1: heroSmall3, small2: heroSmall4 },
   ]);
   const [currentHeroIndex, setCurrentHeroIndex] = useState(0);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-
-  // Preload all images before starting the animation
-  useEffect(() => {
-    const imageUrls = heroImages
-      .map((hero) => Object.values(hero))
-      .reduce((acc, val) => acc.concat(val), [])
-      .filter(Boolean) as string[];
-
-    const imagePromises = imageUrls.map((imageUrl) => {
-      return new Promise<void>((resolve, reject) => {
-        const img = new Image();
-        img.src = imageUrl;
-        img.onload = () => resolve();
-        img.onerror = () => reject();
-      });
-    });
-
-    Promise.all(imagePromises)
-      .then(() => {
-        setImagesLoaded(true);
-      })
-      .catch(() => {
-        console.error("Image preloading failed.");
-      });
-  }, [heroImages]);
 
   useEffect(() => {
-    if (!imagesLoaded) {
-      return; // Don't start the animation until all images are loaded
-    }
-
     const interval = setInterval(() => {
-      setCurrentHeroIndex(
-        (prevIndex) => (prevIndex + 1) % (heroImages.length * 2)
-      );
-    }, 3000); // Total transition time for all images
+      setCurrentHeroIndex((prevIndex) => (prevIndex + 1) % heroImages.length);
+    }, 3000);
 
     return () => clearInterval(interval);
-  }, [heroImages, imagesLoaded]);
+  }, [heroImages]);
 
-  const currentHero = heroImages[currentHeroIndex % 2];
-
-  const transitionTime = 1000; // Faster transition time
-  const delaySmall1 = 0;
-  const delayHeroBig = transitionTime;
-  const delaySmall2 = transitionTime * 2;
+  const currentHero = heroImages[currentHeroIndex];
 
   return (
     <>
       <section className="w-full">
         <div className="flex flex-col">
           <div className="flex w-full max-w-full">
-            <img
-              src={currentHero.small2}
-              alt=""
-              className="w-[50%]"
-              style={{
-                transition: `opacity ${transitionTime}ms ease-in-out`,
-                opacity: currentHeroIndex % 2 === 0 ? 1 : 0,
-              }}
-            />
-            <img
-              src={currentHero.small1}
-              alt=""
-              className="w-[50%]"
-              style={{
-                transition: `opacity ${transitionTime}ms ease-in-out`,
-                opacity: currentHeroIndex % 2 === 1 ? 1 : 0,
-              }}
-            />
+            <img src={currentHero.small1} alt="" className="w-[50%]" />
+            <img src={currentHero.small2} alt="" className="w-[50%]" />
           </div>
           <div className="flex">
-            <img
-              src={currentHeroIndex % 2 === 0 ? heroBig1 : heroBig2}
-              alt=""
-              className="w-full"
-              style={{
-                transition: `opacity ${transitionTime}ms ease-in-out`,
-                opacity: 1,
-              }}
-            />
+            <img src={currentHero.big} alt="" className="w-full" />
           </div>
         </div>
       </section>
