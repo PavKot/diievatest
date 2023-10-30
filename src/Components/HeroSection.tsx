1f276c2d5e64b81303fa401bae475e24f71ab624
PavKot <pavlo.melnik.20043@gmail.com>
2023-10-30 17:51:15 +0200

    animation

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,24 +1,101 @@
-import React from "react";
-import heroImg from "../Assets/heroImg.png";
-import heroImgNew from "../Assets/heroImgNew.png";
-import banner from "../Assets/banner.png";
-import bannermob from "../Assets/bannermob.png";
+import React, { useState, useEffect } from "react";
+import heroBig1 from "../Assets/heroBig1.png";
+import heroBig2 from "../Assets/heroBig2.png";
+import heroSmall1 from "../Assets/heroSmall1.png";
+import heroSmall2 from "../Assets/heroSmall2.png";
+import heroSmall3 from "../Assets/heroSmall3.png";
+import heroSmall4 from "../Assets/heroSmall4.png";
 
 const HeroSection = () => {
+  const [heroImages, setHeroImages] = useState([
+    { big: heroBig1, small1: heroSmall1, small2: heroSmall2 },
+    { big: heroBig2, small1: heroSmall3, small2: heroSmall4 },
+  ]);
+  const [currentHeroIndex, setCurrentHeroIndex] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  // Preload all images before starting the animation
+  useEffect(() => {
+    const imageUrls = heroImages
+      .map((hero) => Object.values(hero))
+      .reduce((acc, val) => acc.concat(val), [])
+      .filter(Boolean) as string[];
+
+    const imagePromises = imageUrls.map((imageUrl) => {
+      return new Promise<void>((resolve, reject) => {
+        const img = new Image();
+        img.src = imageUrl;
+        img.onload = () => resolve();
+        img.onerror = () => reject();
+      });
+    });
+
+    Promise.all(imagePromises)
+      .then(() => {
+        setImagesLoaded(true);
+      })
+      .catch(() => {
+        console.error("Image preloading failed.");
+      });
+  }, [heroImages]);
+
+  useEffect(() => {
+    if (!imagesLoaded) {
+      return; // Don't start the animation until all images are loaded
+    }
+
+    const interval = setInterval(() => {
+      setCurrentHeroIndex(
+        (prevIndex) => (prevIndex + 1) % (heroImages.length * 2)
+      );
+    }, 3000); // Total transition time for all images
+
+    return () => clearInterval(interval);
+  }, [heroImages, imagesLoaded]);
+
+  const currentHero = heroImages[currentHeroIndex % 2];
+
+  const transitionTime = 1000; // Faster transition time
+  const delaySmall1 = 0;
+  const delayHeroBig = transitionTime;
+  const delaySmall2 = transitionTime * 2;
+
   return (
     <>
-      <div className="banner fixed left-0 right-0 mx-auto top-[25vh] z-[999] hidden md:block">
-        <a href="https://www.instagram.com/diieva.brand/">
-          <img src={banner} alt="" className="mx-auto" />
-        </a>
-      </div>
-      <div className="banner fixed left-0 right-0 mx-auto top-[25vh] z-[999] block md:hidden">
-        <a href="https://www.instagram.com/diieva.brand/">
-          <img src={bannermob} alt="" className="mx-auto" />
-        </a>
-      </div>
-      <section className="w-[100%]">
-        <img src={heroImgNew} alt="Hero Image" className="w-[100%]" />
+      <section className="w-full">
+        <div className="flex flex-col">
+          <div className="flex w-full max-w-full">
+            <img
+              src={currentHero.small2}
+              alt=""
+              className="w-[50%]"
+              style={{
+                transition: `opacity ${transitionTime}ms ease-in-out`,
+                opacity: currentHeroIndex % 2 === 0 ? 1 : 0,
+              }}
+            />
+            <img
+              src={currentHero.small1}
+              alt=""
+              className="w-[50%]"
+              style={{
+                transition: `opacity ${transitionTime}ms ease-in-out`,
+                opacity: currentHeroIndex % 2 === 1 ? 1 : 0,
+              }}
+            />
+          </div>
+          <div className="flex">
+            <img
+              src={currentHeroIndex % 2 === 0 ? heroBig1 : heroBig2}
+              alt=""
+              className="w-full"
+              style={{
+                transition: `opacity ${transitionTime}ms ease-in-out`,
+                opacity: 1,
+              }}
+            />
+          </div>
+        </div>
       </section>
     </>
   );
